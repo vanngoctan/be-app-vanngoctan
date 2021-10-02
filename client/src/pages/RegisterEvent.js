d8f1cd97a9213f14946060cbb971d1a39059f08e
Tan Van <ngoctan.van.cs@gmail.com>
2021-10-02 16:10:19 +0700

    - Update form for each type of event.

--- client/src/pages/RegisterEvent.js
+++ client/src/pages/RegisterEvent.js
@@ -1,27 +1,93 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default function RegisterEvent() {
+  const [Event, setEvent] = useState([]);
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/events/${id}`).then((response) => {
+      setEvent(response.data);
+      console.log(response.data);
+    });
+  }, [id]);
+
+  if (Event.needInfo === "worklocation") {
+    const initialValues = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      workLocation: "",
+    };
+  } else {
+    const initialValues = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      hobbies: "",
+    };
+  }
+
   return (
     <div className="register-user-to-event">
       <Formik>
         <Form className="form-container">
           <label>First name: </label>
-          <Field className="input" id="inputFirstName" name="firstName" placeholder="John" autocomplete="off"/>
+          <Field
+            className="input"
+            id="inputFirstName"
+            name="firstName"
+            placeholder="John"
+            autoComplete="off"
+          />
 
           <label>Last name: </label>
-          <Field className="input" id="inputLastName" name="lastName" placeholder="Smith" autocomplete="off"/>
+          <Field
+            className="input"
+            id="inputLastName"
+            name="lastName"
+            placeholder="Smith"
+            autoComplete="off"
+          />
 
           <label>Email: </label>
-          <Field className="input" id="inputEmail" name="email" placeholder="john@example.com" autocomplete="off"/>
+          <Field
+            className="input"
+            id="inputEmail"
+            name="email"
+            placeholder="john@example.com"
+            autoComplete="off"
+          />
 
-          <label>Work Location: </label>
-          <Field className="input" id="inputWorkLocation" name="workLocation" placeholder="" autocomplete="off"/>
+          {Event.needInfo === "worklocation" && (<label>Work Location: </label>)}
+          {Event.needInfo === "worklocation" && (
+            <Field
+              className="input"
+              id="inputWorkLocation"
+              name="workLocation"
+              placeholder=""
+              autoComplete="off"
+            />
+          )}
 
-          <label>Hobbies: </label>
-          <Field className="input" id="inputHobbies" name="hobbies" placeholder="" autocomplete="off"/>
+          {Event.needInfo !== "worklocation" && (<label>Hobbies: </label>)}
+          {Event.needInfo !== "worklocation" && (
+            <Field
+              className="input"
+              id="inputHobbies"
+              name="hobbies"
+              placeholder=""
+              autoComplete="off"
+            />
+          )}
 
-          <button className="join-btn" type="submit">Register</button>
+          <button className="join-btn" type="submit">
+            Register
+          </button>
         </Form>
       </Formik>
     </div>
