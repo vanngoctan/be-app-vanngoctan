d026fec7dbf4881ca5e218397fabdea6393fa795
Tan Van <ngoctan.van.cs@gmail.com>
2021-10-02 15:24:52 +0700

    - Update UI of RegisterEvent.

--- client/src/pages/RegisterEvent.js
+++ client/src/pages/RegisterEvent.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default function RegisterEvent() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="register-user-to-event">
+      <Formik>
+        <Form className="form-container">
+          <label>First name: </label>
+          <Field className="input" id="inputFirstName" name="firstName" placeholder="John" autocomplete="off"/>
+
+          <label>Last name: </label>
+          <Field className="input" id="inputLastName" name="lastName" placeholder="Smith" autocomplete="off"/>
+
+          <label>Email: </label>
+          <Field className="input" id="inputEmail" name="email" placeholder="john@example.com" autocomplete="off"/>
+
+          <label>Work Location: </label>
+          <Field className="input" id="inputWorkLocation" name="workLocation" placeholder="" autocomplete="off"/>
+
+          <label>Hobbies: </label>
+          <Field className="input" id="inputHobbies" name="hobbies" placeholder="" autocomplete="off"/>
+
+          <button className="join-btn" type="submit">Register</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
