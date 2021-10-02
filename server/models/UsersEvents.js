9cc9bccf17fbd77a2248035e50a6027ac4160a2b
Tan Van <ngoctan.van.cs@gmail.com>
2021-10-03 00:03:31 +0700

    + Add model UsersEvents

---
+++ server/models/UsersEvents.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+  const UsersEvents = sequelize.define("Users_Events", {
+    custom: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
+  });
+
+  return UsersEvents;
+};
