f0a50f1ed2f81c3edf78c8cc683f73e76c6a93c7
Tan Van <ngoctan.van.cs@gmail.com>
2021-10-03 01:06:21 +0700

    + add new Component for viewing the users who registered for an event.

---
+++ client/src/pages/ViewRegisterEvent.js
@@ -0,0 +1,105 @@
+import React from "react";
+import Pagination from "pagination-component";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function ViewRegisterEvent() {
+  const [Event, setEvent] = useState([]);
+  const [ListOfUsers, setListOfUsers] = useState([]);
+  const [Page, setPage] = useState({ currentPage: 1, count: 0, maxPage: 1 });
+
+  let { eventId } = useParams();
+
+  const loadUser = (page) => {
+    axios
+      .get(`http://localhost:3001/view/${eventId}/page/${page}`)
+      .then((response) => {
+        setListOfUsers(response.data.result);
+        setPage({
+          currentPage: response.data.current,
+          count: response.data.count,
+          maxPage: response.data.pages,
+        });
+      });
+  };
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/events/${eventId}`).then((response) => {
+      setEvent(response.data);
+    });
+
+    axios
+      .get(`http://localhost:3001/view/${eventId}/page/${Page.currentPage}`)
+      .then((response) => {
+        setListOfUsers(response.data.result);
+        setPage({
+          currentPage: response.data.current,
+          count: response.data.count,
+          maxPage: response.data.pages,
+        });
+      });
+  }, [eventId, Page.currentPage]);
+
+  return (
+    <div>
+      <h1>List of Users registered for {Event.name}</h1>
+
+      <Pagination
+        initialPage={1}
+        show={5}
+        pageCount={Page.maxPage}
+        onChange={(page) => loadUser(page)}
+      >
+        {({
+          setPage,
+          page,
+          index,
+          currentPage,
+          isPrev,
+          isNext,
+          isCurrentPage,
+        }) => {
+          if (isPrev)
+            return (
+              <button
+                className="join-btn page-style"
+                onClick={() => {
+                  setPage({ prev: true });
+                }}
+              >
+                Previous
+              </button>
+            );
+
+          if (isNext)
+            return (
+              <button
+                className="join-btn page-style"
+                onClick={() => {
+                  setPage({ next: true });
+                }}
+              >
+                Next
+              </button>
+            );
+
+          return (
+            <div
+              key={index}
+              className="page-style"
+              style={{
+                backgroundColor: isCurrentPage ? "lightskyblue" : "white",
+              }}
+              onClick={() => {
+                setPage({ page });
+              }}
+            >
+              <h5>{page}</h5>
+            </div>
+          );
+        }}
+      </Pagination>
+    </div>
+  );
+}
